ea4f25b0fa444e96f09d11b726b223983ed13303
Cassey Lottman <clottman@users.noreply.github.com>
2021-02-03 15:10:04 -0600

    more styling

--- app/javascript/src/tailwind.config.js
+++ app/javascript/src/tailwind.config.js
@@ -3,11 +3,6 @@ const colors = require('tailwindcss/colors')
 module.exports = {
   // purge: [],
   darkMode: false, // or 'media' or 'class'
-  theme: {
-    colors: {
-      green: colors.coolGray
-    },
-  },
   variants: {
     extend: {},
   },
